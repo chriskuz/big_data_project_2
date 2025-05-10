03272dba8551bfd2de71ec2bab667d69d25b60cb
ali <Alinazim363@gmail.com>
2025-05-09 20:39:37 -0400

    Update diagnose_heart.sh

--- src/part_2/diagnose_heart.sh
+++ src/part_2/diagnose_heart.sh
@@ -3,4 +3,4 @@ source ../../../../env.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /spark-project2/input/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /spark-project2/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../data/framingham.csv /spark-project2/input/
-/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./diagnose_heart.py hdfs://$SPARK_MASTER:9000/spark-project2/input/
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./diagnose_heart.py hdfs://$SPARK_MASTER:9000/spark-project2/input/framingham.csv
