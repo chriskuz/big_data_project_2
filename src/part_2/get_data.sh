b2cb42e0deb5cd3629902001d1cd36a94ddace99
Chris Kuzemka <cmkuzemka@gmail.com>
2025-05-09 21:03:16 -0400

    adjusted message.

--- src/part_2/get_data.sh
+++ src/part_2/get_data.sh
@@ -11,7 +11,7 @@ mkdir -p "$DATA_DIR"
 cd "$DATA_DIR" || { echo: "Failed data directory access"; exit 1; }
 
 
-echo "Downloading NBA shot logs ZIP from Kaggle"
+echo "Downloading Framingham Data"
 curl -L -u "${KAGGLE_USER}:${KAGGLE_KEY}" \
   -o "$ZIP_NAME" \
   "https://www.kaggle.com/api/v1/datasets/download/aasheesh200/framingham-heart-study-dataset"
