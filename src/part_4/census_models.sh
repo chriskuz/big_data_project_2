06885376735f0bef38d58bea1b4a5686b8842e88
ali <Alinazim363@gmail.com>
2025-05-10 22:08:05 -0400

    Update census_models.sh

--- src/part_4/census_models.sh
+++ src/part_4/census_models.sh
@@ -6,6 +6,6 @@ source ../../../../env.sh
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../data/train3.data /spark-project2/input/
 /usr/local/spark/bin/spark-submit \
     --master spark://$SPARK_MASTER:7077 \
-    ./census_income.py \
+    ./census_models.py \
     hdfs://$SPARK_MASTER:9000/spark-project2/input/train3.data \
     hdfs://$SPARK_MASTER:9000/spark-project2/input/test3.test
